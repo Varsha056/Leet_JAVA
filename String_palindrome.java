12ed27b47b7c37804e4f76a3f0c686e687443612
Varsha Gangadhar <54168694+Varsha056@users.noreply.github.com>
2024-12-06 21:17:36 +0530

    Create String_palindrome.java

---
+++ String_palindrome.java
@@ -0,0 +1,27 @@
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+// Given a string s, return true if it is a palindrome, or false otherwise.
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+
+        while (start <= end) {
+
+            if (!Character.isLetterOrDigit(s.charAt(start))) {
+                start++;
+            } else if (!Character.isLetterOrDigit(s.charAt(end))) {
+                end--;
+            } else {
+                if (Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) {
+                    return false;
+                }
+                start++;
+                end--;
+            }
+
+        }
+        return true;
+    }
+}
