a5a4cdd68379b6ebb1dd3ea90919dc91a2009d1c
Varsha Gangadhar <54168694+Varsha056@users.noreply.github.com>
2024-12-13 15:40:42 +0530

    Update hasLoop.java

--- hasLoop.java
+++ hasLoop.java
@@ -1,3 +1,15 @@
+/*
+Create two pointers, slow and fast, both initially pointing to the head of the linked list.
+
+Traverse the list with the slow pointer moving one step at a time, while the fast pointer moves two steps at a time.
+
+If there is a loop in the list, the fast pointer will eventually meet the slow pointer. If this occurs, the method should return true.
+
+If the fast pointer reaches the end of the list or encounters a null value, it means there is no loop in the list. In this case, the method should return false.
+
+
+
+*/
 public class LinkedList {
 
     private Node head;
